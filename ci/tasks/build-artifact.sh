25ca6a2e42cabcb3f7f186da833e5f828731bc88
Keith Strini <kstrini@pivotal.io>
2016-03-02 14:20:32 -0500

    removing version

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -4,9 +4,9 @@ set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 BINARY_DIR=$PWD/binary
 
-version=`cat version/number`
+#version=`cat version/number`
 cd code-repo
-gradle assemble -PVersion=$version
-
+#gradle assemble -PVersion=$version
+gradle assemble
 mv build/libs/*.war $BINARY_DIR
 ls $BINARY_DIR
